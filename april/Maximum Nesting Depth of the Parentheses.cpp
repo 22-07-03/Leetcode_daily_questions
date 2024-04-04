dbb200eb0012a5b855cf8a72f7ee3f9edef251c3
Praizy <121686362+22-07-03@users.noreply.github.com>
2024-04-04 15:06:06 +0530

    Create Maximum Nesting Depth of the Parentheses.cpp
     1. use stack for this kind of problems

---
+++ april/Maximum Nesting Depth of the Parentheses.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int ans=0;
+        stack<int>st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                st.push(s[i]);
+            }
+            else if(s[i]==')'){ st.pop();}
+            ans = max(ans , (int)st.size());
+        }
+        return ans;
+    }
+};
