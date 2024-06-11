8b2ec64288206a007200c5c842854618ae4e0a43
Praizy <121686362+22-07-03@users.noreply.github.com>
2024-06-12 00:03:27 +0530

    1122. Relative Sort Array.cpp

---
+++ June/1122. Relative Sort Array.cpp
@@ -0,0 +1,31 @@
+#pragma GCC optimize("03")
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+        vector<int> result;
+        // Traverse through the relative order array
+        for (int i = 0; i < arr2.size(); i++) {
+            // Traverse through the target array
+            for (int j = 0; j < arr1.size(); j++) {
+                // If element in target array matches with
+                // relative order element
+                if (arr1[j] == arr2[i]) {
+                    // Add it to the result array
+                    result.push_back(arr1[j]);
+                    // Mark the element in target array as visited
+                    arr1[j] = -1;
+                }
+            }
+        }
+        // Sort the remaining elements in the target array
+        sort(arr1.begin(), arr1.end());
+        // Add the remaining elements to the result array
+        for (int i = 0; i < arr1.size(); i++) {
+            if (arr1[i] != -1) {
+                result.push_back(arr1[i]);
+            }
+        }
+        return result;
+    }
+};
